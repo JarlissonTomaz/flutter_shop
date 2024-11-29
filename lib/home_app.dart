0bea93eaa0921c909406d11d11d361684cada733
JarlissonTomaz <tomazj995@gmail.com>
2024-11-28 21:11:24 -0300

    estrutura basica

---
+++ lib/home_app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomeApp extends StatefulWidget {
+  const HomeApp({super.key});
+
+  @override
+  State<HomeApp> createState() => _HomeAppState();
+}
+
+class _HomeAppState extends State<HomeApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+    );
+  }
+}
